de6553ba967e224ef8de96bffd3aab717d3b1a13
Aala76 <62723840+Aala76@users.noreply.github.com>
2021-12-13 10:23:47 -0500

    Update Airpoll.py

--- Airpoll.py
+++ Airpoll.py
@@ -6,20 +6,19 @@ import plotly.io as pio
 
 
 airdata = pd.read_csv('Air_Quality.csv')
-asthdata = pd.read_csv('Asthma_Emergency_Department_Visits.csv')
-
 
 airdata = airdata.drop('Message', 1)
 airdata = airdata[(airdata['Name'] == 'Fine Particulate Matter (PM2.5)') |
    (airdata['Name'] == 'Nitrogen Dioxide (NO2)') |
-   (airdata['Name'] == 'Ozone (O3)')]
+   (airdata['Name'] == 'Ozone (O3)')|
+   (airdata['Name'] == 'Sulfur Dioxide (SO2)')]
 
 airdata['Start_Date'] = airdata['Start_Date'].apply(pd.to_datetime)
 airdata['Start_Date'] = airdata['Start_Date'].dt.year
 
 airdata.drop(['Time Period', 'Indicator ID', 'Unique ID'], axis=1)
 airdata= airdata[(airdata['Start_Date'] <=2015) & (airdata['Start_Date'] >=2009)]
-airdata = airdata.rename({'Start_Date': 'TimeFrame', 'Geo Place Name': 'Location', 'Name': 'Pollutant'}, axis=1)
+airdata = airdata.rename({'Start_Date': 'TimeFrame', 'Geo Place Name': 'Location', 'Name': 'Pollutant', 'Data Value': 'Mean Pollutant Value'}, axis=1)
 airdata = airdata.drop(columns=['Unique ID', 'Indicator ID', 'Measure', 'Time Period'])
 airdata = airdata[airdata['Geo Type Name'] == 'CD']
 
@@ -37,6 +36,15 @@ asth = asth[(asth['Location'] != 'New York City')]
 asth = asth[(asth['Location'] != 'Manhattan')]
 asth['Geo Join ID'] = asth['Geo Join ID'].map(lambda x: x.lstrip('uhf'))
 asth['Geo Join ID']=asth['Geo Join ID'].astype(int)
+asth= asth[(asth['TimeFrame'] <=2015) & (asth['TimeFrame'] >=2009)]
+asth = asth.rename(columns={'Data': 'Asthma Emergency Dep. Visits'})
+
+nycb = json.load(open('UHF42.json', 'r'))
+Boro_map = {}
+for feature in nycb['features']:
+    feature['Borough'] = feature['properties']['BOROUGH']
+    Boro_map[feature['properties']['GEOCODE']] = feature['Borough']
+asth['Borough'] = asth['Geo Join ID'].map(Boro_map)
 
 
 def Boroughdata(Borough):
@@ -54,11 +62,30 @@ def Boroughdata(Borough):
 
 def Bmap(data):
     nycb = json.load(open('neigh.json', 'r'))
-    fig = px.choropleth_mapbox(data, locations='Geo Join ID', geojson=nycb, color='Data Value', hover_name='Location', mapbox_style='carto-positron', center = {'lat':40.681395467965096, 'lon':-73.93646399798689}, zoom=9, opacity = 0.5)
+    fig = px.choropleth_mapbox(data, locations='Geo Join ID', geojson=nycb, color='Mean Pollutant Value', hover_name='Location', mapbox_style='carto-positron', center = {'lat':40.681395467965096, 'lon':-73.93646399798689}, zoom=9, opacity = 0.5)
     return fig
 
 
-def Piech(year, data):
+def Piech(year, data, Borough):
     df = data.query("TimeFrame == {}".format(year))
-    fig = px.pie(df, values='Data Value', names='Pollutant', title='Borough Pollutants in ' + year)
+    fig = px.pie(df, values='Mean Pollutant Value', names='Pollutant', title=Borough +' Pollutants in ' + year)
+    return fig
+
+def asthmamap(asth):
+    nyc = json.load(open('boroughsgeojson.json', 'r'))
+    Boro = {}
+    y = asth.dropna()
+    for feature in nyc['features']:
+        feature['id'] = feature['properties']['BoroCode']
+        Boro[feature['properties']['BoroName']] = feature['id']
+    y['BoroughID'] = y['Borough'].apply(lambda x: Boro[x])
+
+    location = 'Geo Join ID'
+    geo = nycb
+    hover = 'Location'
+    fig = px.choropleth_mapbox(y, locations=location, geojson=geo, color='Asthma Emergency Dep. Visits', hover_name=hover, mapbox_style='carto-positron', center = {'lat':40.681395467965096, 'lon':-73.93646399798689}, zoom=8, opacity = 0.5)
+    return fig
+
+def nycpie():
+    fig = px.pie(airdata, values='Mean Pollutant Value', names='Pollutant', title='Pollutants in NYC')
     return fig
